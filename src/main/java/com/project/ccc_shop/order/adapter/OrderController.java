52edd3a63b24f06ba88685044d4f94e42db7104f
tzulin <tzulin0517@gmail.com>
2021-12-04 21:18:51 +0800

    added updateOrder in OrderController to receive api request

--- src/main/java/com/project/ccc_shop/order/adapter/OrderController.java
+++ src/main/java/com/project/ccc_shop/order/adapter/OrderController.java
@@ -3,10 +3,16 @@ package com.project.ccc_shop.order.adapter;
 import com.project.ccc_shop.order.usecase.create.CreateOrderInput;
 import com.project.ccc_shop.order.usecase.create.CreateOrderOutput;
 import com.project.ccc_shop.order.usecase.create.CreateOrderUseCase;
+import com.project.ccc_shop.order.usecase.update.UpdateOrderInput;
+import com.project.ccc_shop.order.usecase.update.UpdateOrderOutput;
+import com.project.ccc_shop.order.usecase.update.UpdateOrderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @RestController
 @RequestMapping("/api/order")
@@ -14,30 +20,53 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
 
     CreateOrderUseCase createOrderUseCase;
+    UpdateOrderUseCase updateOrderUseCase;
 
     @Autowired
     public void setCreateOrderUseCase(CreateOrderUseCase createOrderUseCase) {
         this.createOrderUseCase = createOrderUseCase;
     }
 
+    @Autowired
+    public void setUpdateOrderUseCase(UpdateOrderUseCase updateOrderUseCase) {
+        this.updateOrderUseCase = updateOrderUseCase;
+    }
+
     @PostMapping(value = "/create")
-    public ResponseEntity<CreateOrderOutput> createOrder(@RequestBody CreateOrderInput orderInfo) {
+    public ResponseEntity<CreateOrderOutput> createOrder(@RequestBody CreateOrderInput requestBody) {
 
         CreateOrderInput input = new CreateOrderInput();
         CreateOrderOutput output = new CreateOrderOutput();
         try {
-            input.setCustomerId(orderInfo.getCustomerId());
-            input.setShippingFee(orderInfo.getShippingFee());
-            input.setRecipientName(orderInfo.getRecipientName());
-            input.setShippingAddress(orderInfo.getShippingAddress());
-            input.setStatus(orderInfo.getStatus());
-            input.setPaymentMethod(orderInfo.getPaymentMethod());
-            input.setOrderTime(orderInfo.getOrderTime());
-            input.setOrderItems(orderInfo.getOrderItems());
-            input.setSeasoningDiscountCode(orderInfo.getSeasoningDiscountCode());
-            input.setShippingDiscountCode(orderInfo.getShippingDiscountCode());
+            input.setCustomerId(requestBody.getCustomerId());
+            input.setShippingFee(requestBody.getShippingFee());
+            input.setRecipientName(requestBody.getRecipientName());
+            input.setShippingAddress(requestBody.getShippingAddress());
+            input.setStatus(requestBody.getStatus());
+            input.setPaymentMethod(requestBody.getPaymentMethod());
+            input.setOrderTime(requestBody.getOrderTime());
+            input.setOrderItems(requestBody.getOrderItems());
+            input.setSeasoningDiscountCode(requestBody.getSeasoningDiscountCode());
+            input.setShippingDiscountCode(requestBody.getShippingDiscountCode());
 
             this.createOrderUseCase.execute(input, output);
+            return ResponseEntity.status(HttpStatus.OK).body(output);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(output);
+        }
+    }
+
+    @PostMapping(value = "/update")
+    public ResponseEntity<UpdateOrderOutput> updateOrder(@RequestBody UpdateOrderInput requestBody) {
+
+        UpdateOrderInput input = new UpdateOrderInput();
+        UpdateOrderOutput output = new UpdateOrderOutput();
+        try {
+            input.setOrderId(requestBody.getOrderId());
+            input.setStatus(requestBody.getStatus());
+            input.setTime(requestBody.getTime());
+
+            this.updateOrderUseCase.execute(input, output);
             return ResponseEntity.status(HttpStatus.OK).body(output);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(output);
