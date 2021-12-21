a5ba73cc5a88c43892a4eb4a415e5eecac4826b1
IwasKoro <youcry987951@gmail.com>
2021-12-21 14:52:31 +0800

    Add edit seasonings discount use case

--- src/main/java/com/project/ccc_shop/seasonings_discount/adapter/SeasoningsDiscountController.java
+++ src/main/java/com/project/ccc_shop/seasonings_discount/adapter/SeasoningsDiscountController.java
@@ -1,10 +1,15 @@
 package com.project.ccc_shop.seasonings_discount.adapter;
 
+import com.project.ccc_shop.order.usecase.update.UpdateOrderInput;
+import com.project.ccc_shop.order.usecase.update.UpdateOrderOutput;
 import com.project.ccc_shop.seasonings_discount.usecase.create.CreateSeasoningsDiscountInput;
 import com.project.ccc_shop.seasonings_discount.usecase.create.CreateSeasoningsDiscountOutput;
 import com.project.ccc_shop.seasonings_discount.usecase.create.CreateSeasoningsDiscountUseCase;
 import com.project.ccc_shop.seasonings_discount.usecase.get_current.GetCurrentSeasoningsDiscountOutput;
 import com.project.ccc_shop.seasonings_discount.usecase.get_current.GetCurrentSeasoningsDiscountUseCase;
+import com.project.ccc_shop.seasonings_discount.usecase.edit.EditSeasoningsDiscountInput;
+import com.project.ccc_shop.seasonings_discount.usecase.edit.EditSeasoningsDiscountOutput;
+import com.project.ccc_shop.seasonings_discount.usecase.edit.EditSeasoningsDiscountUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +22,7 @@ public class SeasoningsDiscountController {
 
     CreateSeasoningsDiscountUseCase createSeasoningsDiscountUseCase;
     GetCurrentSeasoningsDiscountUseCase getCurrentSeasoningsDiscountUseCase;
+    EditSeasoningsDiscountUseCase editSeasoningsDiscountUseCase;
 
     @Autowired
     public void setCreateDiscountUseCase(CreateSeasoningsDiscountUseCase createSeasoningsDiscountUseCase) {
@@ -26,6 +32,11 @@ public class SeasoningsDiscountController {
     @Autowired
     public void setGetCurrentSeasoningsDiscountUseCase(GetCurrentSeasoningsDiscountUseCase getCurrentSeasoningsDiscountUseCase) {
         this.getCurrentSeasoningsDiscountUseCase = getCurrentSeasoningsDiscountUseCase;
+    }
+
+    @Autowired
+    public void setEditSeasoningsDiscountUseCase(EditSeasoningsDiscountUseCase editSeasoningsDiscountUseCase) {
+        this.editSeasoningsDiscountUseCase = editSeasoningsDiscountUseCase;
     }
 
     @PostMapping(value = "/create")
@@ -47,6 +58,27 @@ public class SeasoningsDiscountController {
 
         try {
             this.getCurrentSeasoningsDiscountUseCase.execute(output);
+            return ResponseEntity.status(HttpStatus.OK).body(output);
+        } catch (Exception e) {
+            output.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(output);
+        }
+    }
+
+    @PostMapping(value = "/edit")
+    public ResponseEntity<EditSeasoningsDiscountOutput> updateOrder(@RequestBody EditSeasoningsDiscountInput requestBody) {
+
+        EditSeasoningsDiscountInput input = new EditSeasoningsDiscountInput();
+        EditSeasoningsDiscountOutput output = new EditSeasoningsDiscountOutput();
+        try {
+            input.setDiscountCode(requestBody.getDiscountCode());
+            input.setVenderId(requestBody.getVenderId());
+            input.setPolicyDescription(requestBody.getPolicyDescription());
+            input.setStartTime(requestBody.getStartTime());
+            input.setEndTime(requestBody.getEndTime());
+            input.setDiscountRate(requestBody.getDiscountRate());
+
+            this.editSeasoningsDiscountUseCase.execute(input, output);
             return ResponseEntity.status(HttpStatus.OK).body(output);
         } catch (Exception e) {
             output.setMessage(e.getMessage());
---
+++ src/main/java/com/project/ccc_shop/seasonings_discount/usecase/edit/EditSeasoningsDiscountOutput.java
@@ -0,0 +1,9 @@
+package com.project.ccc_shop.seasonings_discount.usecase.edit;
+
+import com.project.ccc_shop.common.Output;
+
+import java.sql.Timestamp;
+
+public class EditSeasoningsDiscountOutput extends Output {
+// no output here
+}
